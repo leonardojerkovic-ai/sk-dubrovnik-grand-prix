674a32254f2623b16b550a0069d63c8278b09e0d
Leona <leona@example.com>
2026-08-29 19:05:33 +0200

    dodana dinamička ruta

--- app/poredak/page.js
+++ app/poredak/page.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+export const dynamic = 'force-dynamic';import { createClient } from '@supabase/supabase-js'
 
 // Inicijalizacija Supabase klijenta
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
